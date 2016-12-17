50033f4ae5df49e995424ef0323a4c5246c72ab4
Cody Soyland <cody@soyland.org>
2016-12-16 18:20:21 -0600

    Move VPC/subnet creation to sandbox template, add additional subnets, make subnet/vpc parameters for pilosa cluster template

--- cfn-pilosa/cfn-pilosa.py
+++ cfn-pilosa/cfn-pilosa.py
@@ -17,6 +17,22 @@ class PilosaTemplate(Skel):
         self.num_agents = num_agents
 
     @cfparam
+    def vpc(self):
+        return Parameter(
+            'VPC',
+            Description='VPC to use for pilosa instance',
+            Type='String',
+        )
+
+    @cfparam
+    def subnet(self):
+        return Parameter(
+            'Subnet',
+            Description='Subnet to use for pilosa instance',
+            Type='String',
+        )
+
+    @cfparam
     def ami(self):
         return Parameter(
             'AMI',
@@ -87,62 +103,11 @@ class PilosaTemplate(Skel):
         )
 
     @cfresource
-    def vpc(self):
-        return ec2.VPC(
-            'PilosaVPC',
-            InstanceTenancy='dedicated',
-            CidrBlock='10.0.0.0/16',
-            EnableDnsHostnames='true',
-            EnableDnsSupport='true',
-        )
-
-    @cfresource
     def hosted_zone(self):
         return route53.HostedZone(
             'PilosaZone',
             Name=Join('', [Ref(self.cluster_name), '.sandbox.pilosa.com']),
             VPCs=[route53.HostedZoneVPCs(VPCId=Ref(self.vpc), VPCRegion=Ref('AWS::Region'))])
-
-    @cfresource
-    def subnet(self):
-        return ec2.Subnet(
-            'Subnet',
-            CidrBlock='10.0.0.0/24',
-            VpcId=Ref(self.vpc),
-            Tags=Tags(Application=Ref('AWS::StackId')))
-
-    @cfresource
-    def gateway(self):
-        return ec2.InternetGateway('InternetGateway')
-
-    @cfresource
-    def gateway_attachment(self):
-        return ec2.VPCGatewayAttachment(
-            'AttachGateway',
-            VpcId=Ref(self.vpc),
-            InternetGatewayId=Ref(self.gateway))
-
-    @cfresource
-    def route_table(self):
-        return ec2.RouteTable(
-            'RouteTable',
-            VpcId=Ref(self.vpc))
-
-    @cfresource
-    def internet_route(self):
-        return ec2.Route(
-            'Route',
-            DependsOn='AttachGateway',
-            GatewayId=Ref(self.gateway),
-            DestinationCidrBlock='0.0.0.0/0',
-            RouteTableId=Ref(self.route_table))
-
-    @cfresource
-    def subnet_route_table_association(self):
-        return ec2.SubnetRouteTableAssociation(
-            'SubnetRouteTableAssociation',
-            SubnetId=Ref(self.subnet),
-            RouteTableId=Ref(self.route_table))
 
     @cfresource
     def instance_security_group(self):
@@ -155,7 +120,6 @@ class PilosaTemplate(Skel):
                     FromPort='22',
                     ToPort='22',
                     CidrIp='0.0.0.0/0',
-                    #SourceSecurityGroupId=Ref(self.security_group().title)
                 ),
             ],
             VpcId=Ref(self.vpc),
