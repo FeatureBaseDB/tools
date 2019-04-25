ba628edb9cb8bd1bfda5cf695262199b9ead12a1
Matt Jaffee <matthew.jaffee@gmail.com>
2019-04-25 07:49:55 -0500

    light refactoring and add set/time generator test

--- imagine/generators_test.go
+++ imagine/generators_test.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"testing"
+	"time"
+
+	gopilosa "github.com/pilosa/go-pilosa"
 )
 
 func testSequenceGenerator(s sequenceGenerator, min int64, max int64, total int64) error {
@@ -64,4 +68,87 @@ func Test_Generators(t *testing.T) {
 		t.Fatalf("unexpected error: %v", err)
 	}
 	testValueGenerator(perm, -3, 5, 9)
+}
+
+func float64p(v float64) *float64 {
+	return &v
+}
+func uint64p(v uint64) *uint64 {
+	return &v
+}
+func int64p(v int64) *int64 {
+	return &v
+}
+func durationp(v duration) *duration {
+	return &v
+}
+
+func TestNewSetGenerator(t *testing.T) {
+	startTime := time.Date(2000, time.Month(1), 2, 3, 4, 5, 6, time.UTC)
+	dur := time.Hour * 120
+	spec := &taskSpec{
+		FieldSpec: &fieldSpec{
+			Type:         fieldTypeTime,
+			Max:          1,
+			Chance:       float64p(1.0),
+			DensityScale: uint64p(2097152),
+			Density:      1.0,
+		},
+		ColumnOrder:    valueOrderLinear,
+		DimensionOrder: dimensionOrderRow,
+		Columns:        uint64p(10),
+		RowOrder:       valueOrderLinear,
+		Seed:           int64p(0),
+		Stamp:          stampTypeIncreasing,
+		StampStart:     &startTime,
+		StampRange:     durationp(duration(dur)),
+	}
+
+	updateChan := make(chan taskUpdate, 10)
+	go func() {
+		for _, ok := <-updateChan; ok; {
+		}
+	}()
+	sg, err := newSetGenerator(spec, updateChan, "updateid")
+	if err != nil {
+		t.Fatalf("getting new set generator: %v", err)
+	}
+	lastT := int64(0)
+	i := -1
+	endTime := startTime.Add(dur)
+	for r, err := sg.NextRecord(); err != io.EOF; r, err = sg.NextRecord() {
+		if err != nil {
+			t.Fatalf("Error in iterator: %v", err)
+		}
+		col, ok := r.(gopilosa.Column)
+		if !ok {
+			t.Fatalf("%v not a Column", r)
+		}
+		i++
+		if col.RowID != 0 {
+			t.Fatalf("unexpected row at record %d: %v", i, col)
+		}
+		if int(col.ColumnID) != i {
+			t.Fatalf("unexpected col: exp: %d got %d", i, col.ColumnID)
+		}
+		if col.Timestamp <= lastT {
+			t.Fatalf("unexpected... timestamp did not increase: last: %d this: %v", lastT, col)
+		}
+		if lastT >= col.Timestamp {
+			t.Fatalf("time stamp did not increase, last: %d, this: %d", lastT, col.Timestamp)
+		}
+		lastT = col.Timestamp
+		tim := time.Unix(0, col.Timestamp)
+		if tim.Before(startTime) {
+			t.Fatalf("got a time before start time: %v", tim)
+		}
+		if tim.After(endTime) {
+			t.Fatalf("got a time after start+duration: %v", tim)
+		}
+	}
+	if endTime.Sub(time.Unix(0, lastT)) > dur/2 {
+		t.Fatalf("less than half the duration was used - lastT: %v", lastT)
+	}
+
+	close(updateChan)
 }
